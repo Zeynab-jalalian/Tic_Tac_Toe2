0c01fd405c742adcdba9df24a35f79033a765ead
Zeynab Jalalian <zjalalianweb@gmail.com>
2025-10-05 12:38:35 +0330

    pc

--- script.js
+++ script.js
@@ -2,7 +2,11 @@ const btns = document.querySelectorAll(".btn");
 const msg = document.getElementById("msg");
 const msgbox = document.querySelector(".msgbox");
 const newGame = document.querySelector(".newGame");
-const restart=document.querySelector(".restart");
+const restart = document.querySelector(".restart");
+const friend = document.getElementById("friend");
+const wrapper = document.querySelector(".wrapper");
+const container = document.querySelector(".container");
+const single = document.getElementById("single");
 
 let Xturn = true;
 let count = 0;
@@ -17,27 +21,67 @@ let winningPattern = [
   [2, 4, 6],
 ];
 
+let singlePlayer = false;
+
+single.addEventListener("click", () => {
+  wrapper.classList.remove("hide");
+  container.classList.add("hide");
+  singlePlayer = true;
+});
+
+function computerMove() {
+  const emptyBtns = Array.from(btns).filter((btn) => btn.innerText === "");
+  if (emptyBtns.length === 0) return;
+  const randomBtn = emptyBtns[Math.floor(Math.random() * emptyBtns.length)];
+  randomBtn.innerText = "O";
+  randomBtn.disabled = true;
+  randomBtn.style.color = "blue";
+  count += 1;
+  winChecker();
+  if (count === 9) {
+    disable();
+    draw();
+  }
+}
+
 function disable() {
   msgbox.classList.remove("hide");
 }
 function whoWins(val1) {
-  msg.innerText = `${val1} Wins!`;
+  if (singlePlayer) {
+    if (val1 === "X") {
+      msg.innerText = "You Win!";
+    } else {
+      msg.innerText = "Computer Wins!";
+    }
+  } else {
+    msg.innerText = `${val1} Wins!`;
+  }
+  disable();
 }
 function draw() {
-  msg.innerText = `It's a draw!`;
+    msg.innerText = "It's a draw!";
+    disable();
 }
 function able() {
-    btns.forEach((btn) => {
-      btn.innerText = "";
-      btn.disabled = false;
-    });
-    msgbox.classList.add("hide");
-    count = 0; 
-    Xturn = true;
+  btns.forEach((btn) => {
+    btn.innerText = "";
+    btn.disabled = false;
+  });
+  msgbox.classList.add("hide");
+  wrapper.classList.add("hide");
+  container.classList.remove("hide");
+  count = 0;
+  Xturn = true;
 }
 
 newGame.addEventListener("click", able);
 restart.addEventListener("click", able);
+
+friend.addEventListener("click", () => {
+  wrapper.classList.remove("hide");
+  container.classList.add("hide");
+});
 
 function winChecker() {
   for (let i of winningPattern) {
@@ -62,17 +106,22 @@ btns.forEach((btn) => {
       Xturn = false;
       btn.innerText = "X";
       btn.disabled = true;
-      btn.style.color="red";
+      btn.style.color = "red";
     } else {
       Xturn = true;
       btn.innerText = "O";
       btn.disabled = true;
-       btn.style.color="blue";
+      btn.style.color = "blue";
     }
     count += 1;
     if (count == 9) {
-      disable();
       draw();
+    }
+    if (singlePlayer) {
+      setTimeout(() => {
+        computerMove();
+        Xturn = true;
+      }, 300);
     }
     winChecker();
   });
